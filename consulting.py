1b639a48aa674fa1ee6c48d777c7aa04ed41bc70
park min joo <alsxxxz@dgu.ac.kr>
2025-07-08 16:13:54 +0900

    SentimentAnalyzer(감정 분석) Outout 60개 > 39 로 수정
    0708 피드백 반영하여 다소 극단적인 감정 분석 결과는 제거 하였음.

--- consulting.py
+++ consulting.py
@@ -2,16 +2,83 @@ import torch
 from transformers import pipeline, PreTrainedTokenizerFast, BartForConditionalGeneration, TextClassificationPipeline, BertForSequenceClassification, AutoTokenizer
 from sentence_transformers import SentenceTransformer
 import kss
+#2025.07.08 SentimentAnalyzer(감정 분석) Outout 60개 > 39 로 수정
+labels = [
+    "분노",
+    "좌절한",
+    "짜증내는",
+    "방어적인",
+     "슬픔",
+     "실망한",
+     "후회되는",
+     "우울한",
+     "눈물이 나는",
+     "불안",
+     "두려운",
+     "스트레스 받는",
+     "혼란스러운",
+     "당혹스러운",
+     "걱정스러운",
+     "조심스러운",
+     "상처",
+     "질투하는",
+     "충격 받은",
+     "억울한",
+     "괴로워하는",
+     "버려진",
+     "당황",
+     "남의 시선을 의식하는",
+     "외로운",
+     "죄책감의",
+     "부끄러운",
+     "한심한",
+     "혼란스러운(당황한)",
+     "기쁨",
+     "감사하는",
+     "신뢰하는",
+     "편안한",
+     "만족스러운",
+     "흥분",
+     "느긋",
+     "안도",
+     "신이 난",
+     "자신하는"
+]
+label2id = {l: i for i, l in enumerate(labels)}
+id2label = {i: l for l, i in label2id.items()}
+num_labels = len(labels)
+
 
 class SentimentAnalyzer:
     def __init__(self):
         model_name = 'hun3359/klue-bert-base-sentiment'
-        self.analyzer = pipeline('sentiment-analysis', model=model_name, return_all_scores=True)
+
+        #1. 토크나이저 로드
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+        #2.num_labels, id2label, label2id 덮어쓰기
+        model = BertForSequenceClassification.from_pretrained(
+            model_name,
+            num_labels=num_labels,
+            id2label=id2label,
+            label2id=label2id
+        )
+
+        #3. 파이프라인 생성
+        self.analyzer = TextClassificationPipeline(
+            model=model,
+            tokenizer=tokenizer,
+            return_all_scores=True,
+            function_to_apply='softmax'
+        )
 
     def analyze(self, text):
         results = self.analyzer(text)[0]
         top_3_results = sorted(results, key=lambda x: x['score'], reverse=True)[:3]
         return top_3_results
+
+
+
 
 class SimilarityCalculator:
     def __init__(self):
